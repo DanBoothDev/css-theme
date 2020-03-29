8713c73f84d49f79d09e112b2fb1cccd133d3284
Dan Booth <DanBoothDev@gmail.com>
2020-03-29 12:01:21 -0300

    Save the theme in local cache storage

--- public/app.js
+++ public/app.js
@@ -1,27 +1,43 @@
+// STORAGE KEYS
+const THEME = 'theme';
+const IS_SOLAR = 'isSolarTheme';
+
 // DOM
 const darkButton = document.getElementById('dark');
 const lightButton = document.getElementById('light');
 const solarButton = document.getElementById('solar');
 const body = document.body;
 
+// save theme in local cache
+const theme = localStorage.getItem(THEME);
+const isSolar = localStorage.getItem(IS_SOLAR);
+
+if (theme) {
+    body.classList.add(theme);
+    isSolar && body.classList.add('solar'); 
+}
+
 // event handlers
 darkButton.onclick = () => {
     body.classList.replace('light', 'dark');
+    localStorage.setItem(THEME, 'dark');
 };
 
 lightButton.onclick = () => {
     body.classList.replace('dark', 'light');
+    localStorage.setItem(THEME, 'light');
 };
 
 solarButton.onclick = () => {
     if (body.classList.contains('solar') ) {
+        // revert
         body.classList.remove('solar');
-        // update value
         solarButton.style.cssText = `--bg-solar: var(--yellow)`;
+        localStorage.removeItem(IS_SOLAR);
     } else {
+        // set
         body.classList.add('solar');
         solarButton.style.cssText = `--bg-solar: var(--white)`;
-
+        localStorage.setItem(IS_SOLAR, true);
     }
-
 };
